8455eb5c0cab9eb08299abdd661972b8ae562968
Mike Alexandrovsky <mike.alexandrovsky@gmail.com>
2024-06-15 17:45:12 +0300

    hide system ui while in the video player activity

--- app/src/main/java/com/mikealexx/tvsrael/VideoPlayerActivity.java
+++ app/src/main/java/com/mikealexx/tvsrael/VideoPlayerActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
 import com.google.android.exoplayer2.util.MimeTypes;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +28,8 @@ public class VideoPlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+
+        hideSystemUi();
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
@@ -52,6 +55,18 @@ public class VideoPlayerActivity extends AppCompatActivity {
 
         player.prepare();
         player.play();
+    }
+
+    private void hideSystemUi() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+        );
     }
 
     private Map<String, String> buildHeaders() {
